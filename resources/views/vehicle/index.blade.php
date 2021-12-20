14e02cd80502beeec626c74fd34f40baa66795fc
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2021-12-20 15:20:57 +0530

    Debug

--- resources/views/vehicle/index.blade.php
+++ resources/views/vehicle/index.blade.php
@@ -130,8 +130,9 @@
 
             <h2 class="col-md-8">
                 <label class="pull-right">
-                    <form action="{{ route('vehicle.import') }}" method="POST">
+                    <form action="{{ route('vehicle.import') }}" method="POST" enctype="multipart/form-data">
                         @csrf
+                        {{ method_field('POST') }}
 
                         <div class="row">
                             @if (!empty($financeCompanies) && !$financeCompanies->isEmpty())
