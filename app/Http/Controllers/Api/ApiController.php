d0a206785b94d3f06d7e04e7ab4ccb9fbd606fb3
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2022-04-05 12:47:23 +0530

    New changes

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -187,10 +187,13 @@ class ApiController extends BaseController
         $validator = $modal->validators($activityData, NULL, true);
 
         if ($validator->fails()) {
+            \Log::info('Activity error val: ' . $validator->errors()->first());
             return $this->returnError($validator->errors()->first());
         }
 
         $create = $modal::insert($activityData);
+
+        \Log::info('Activity error create: ' . $create);
 
         if ($create) {
             return $this->returnSuccess(__('Record added successfully!'), User::getGlobalResponse($userId));
